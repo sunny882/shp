72a22821cc6f486bacdc91549fe932bc1d028f32
sunny <sany.toor@gmail.com>
2022-05-21 21:08:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+//import logo from './logo.svg';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import Main2 from "./components/Main2";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <Routes>
+          <Route path="main2" element={<Main2 />} />
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import { Router, Route, Routes } from 'react-router-dom'
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
